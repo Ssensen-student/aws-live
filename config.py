b8f2288ea0d34fb9de8e26148f500cd80a9e0a61
Ssensen-student <139458099+Ssensen-student@users.noreply.github.com>
2023-08-28 11:50:09 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.chxepufsqbhr.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "limjiasheng-employee"
+customregion = "us-east-1"
+
